fbfb981188060f0eef3ef9f6c90498ba2dfc116a
Reinoptland <reinoptland@gmail.com>
2021-02-02 19:54:26 +0100

    Cleanup

--- functions.js
+++ functions.js
@@ -6,15 +6,13 @@
 // "Nova" geeft "Hoi Nova!"
 // "Nick" geeft "Hoi Nick!"
 function greet(name) {
-  console.log("AANGEROEPEN?");
-  console.log("INPUT:", name);
   return `Hoi ${name}`;
 }
 
 const greetNova = greet("Nova");
 const greetNick = greet("Nick");
-console.log("OUTPUT 1:", greetNova);
-console.log("OUTPUT 2:", greetNick);
+// console.log("OUTPUT 1:", greetNova);
+// console.log("OUTPUT 2:", greetNick);
 
 /* Opdracht 2 */
 // Schrijf een functie die een hoeveelheid minuten verwacht (als een getal) en teruggeeft
